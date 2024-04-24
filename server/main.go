72c0e8d4e1d5b4401a34a4ab8e44bef07de59250
Sahas Timilsina <tsahas2059@gmail.com>
2024-04-24 20:55:36 +0545

    feat: added dummy restful api

--- server/main.go
+++ server/main.go
@@ -1,4 +1,153 @@
 package main
 
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Staff struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Post string `json:"post"`
+}
+
+type Customer struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Product struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+var (
+	products  []Product
+	staffs    []Staff
+	customers []Customer
+)
+
 func main() {
+	bindAddress := flag.String("addr", ":8080", "Port to run server on")
+	flag.Parse()
+	l := log.New(os.Stdout, "service-api", log.LstdFlags)
+	router := mux.NewRouter()
+
+	// Seed data
+	products = append(products, Product{1, "Transportation", "Service providing transportation from point a to b."}, Product{2, "Communication", "Communication service between consumers."})
+	staffs = append(staffs, Staff{1, "Sahas Timilsina", "CEO"}, Staff{2, "Neo", "CTO"})
+	customers = append(customers, Customer{1, "Samyog Timilsina"}, Customer{2, "Po"})
+
+	router.HandleFunc("/products", getProducts).Methods("GET")
+	router.HandleFunc("/products/{id}", getProduct).Methods("GET")
+	router.HandleFunc("/products", createProduct).Methods("product")
+	router.HandleFunc("/products/{id}", updateProduct).Methods("PUT")
+	router.HandleFunc("/products/{id}", deleteProduct).Methods("DELETE")
+
+	router.HandleFunc("/staffs", getStaffs).Methods("GET")
+	router.HandleFunc("/customers", getCustomers).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(*bindAddress, router))
+	l.Println("Starting server on port ", *bindAddress)
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
+
+func getStaffs(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(staffs)
+}
+
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	for _, product := range products {
+		if product.ID == id {
+			json.NewEncoder(w).Encode(product)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var product Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product.ID = len(products) + 1
+	products = append(products, product)
+	json.NewEncoder(w).Encode(product)
+}
+
+func updateProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var updatedProduct Product
+	err = json.NewDecoder(r.Body).Decode(&updatedProduct)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	for i, product := range products {
+		if product.ID == id {
+			products[i].Name = updatedProduct.Name
+			products[i].Description = updatedProduct.Description
+			json.NewEncoder(w).Encode(products[i])
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func deleteProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	for i, product := range products {
+		if product.ID == id {
+			products = append(products[:i], products[i+1:]...)
+			json.NewEncoder(w).Encode(product)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
 }
